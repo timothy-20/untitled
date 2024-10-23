a99f20363230c2df134186a28b3be6e768d7dc80
timothy-20 <jungun3329@gmail.com>
2024-10-23 18:08:58 +0900

    24.10.23. 1. TKLog4j2Config, log4j 설정 java 클래스로 구성 중.

--- src/main/java/com/timothy/TKLog4j2Config.java
+++ src/main/java/com/timothy/TKLog4j2Config.java
@@ -6,10 +6,15 @@ import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.ConfigurationFactory;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Order;
 import org.apache.logging.log4j.core.config.builder.api.*;
 import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
 
+import java.io.IOException;
+
+@Plugin(name = "TKLog4j2Config", category = ConfigurationFactory.CATEGORY)
+@Order(50)
 public class TKLog4j2Config extends ConfigurationFactory {
     public TKLog4j2Config() {
         super();
@@ -17,15 +22,6 @@ public class TKLog4j2Config extends ConfigurationFactory {
 
     @Override
     public Configuration getConfiguration(LoggerContext loggerContext, ConfigurationSource source) {
-        return null;
-    }
-
-    @Override
-    protected String[] getSupportedTypes() {
-        return new String[] { "*" };
-    }
-
-    public org.apache.logging.log4j.core.config.Configuration configureLog4j2() {
         ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
 
         // 로그 레이아웃 페턴에 대한 설정
@@ -38,25 +34,51 @@ public class TKLog4j2Config extends ConfigurationFactory {
         console.add(patternLayoutBuilder);
         builder.add(console);
 
-        // 로그 파일에 대한 설정
-        AppenderComponentBuilder logFile = builder.newAppender("log", "File");
-        logFile.addAttribute("fileName", "target/logging.log");
+//        // 롤링 로그 파일에 대한 설정
+//        AppenderComponentBuilder rollingLogFile = builder.newAppender("log", "RollingFile");
+//        rollingLogFile.addAttribute("fileName", "logs/record.log");
+//        rollingLogFile.addAttribute("filePattern", "logs/record-%d{yyyy-MM-dd}-%i.log");
+//        rollingLogFile.add(patternLayoutBuilder);
+//
+//        // 롤링 로그 파일에 대한 정책 설정
+//        ComponentBuilder<AppenderComponentBuilder> rollingLogPolicies = builder.newComponent("Policies");
+//        rollingLogPolicies.addComponent(builder.newComponent("CronTriggeringPolicy").addAttribute("schedule", "0 0 0 * * ?"));
+//        rollingLogPolicies.addComponent(builder.newComponent("SizeBasedTriggeringPolicy").addAttribute("size", "100MB"));
+//        rollingLogFile.addComponent(rollingLogPolicies);
+//
+//        // 롤링 로그 파일에 대한 저장 방식(전략) 설정
+//        ComponentBuilder<AppenderComponentBuilder> rollingLogStrategy = builder.newComponent("DefaultRolloverStrategy");
+//        rollingLogStrategy.addAttribute("max", "10");
+//        rollingLogStrategy.addAttribute("fileIndex", "min");
+//        rollingLogStrategy.addAttribute("compressionLevel", "9");
+//        rollingLogStrategy.addAttribute("compressionPattern", "logs/archived-record-%d{yyyy-MM-dd}-%i.zip");
+//        rollingLogFile.addComponent(rollingLogStrategy);
+//        builder.add(rollingLogFile);
 
-        logFile.add(patternLayoutBuilder);
-        builder.add(logFile);
-
-        // 롤링 로그 파일에 대한 설정
-        AppenderComponentBuilder rollingLogFile = builder.newAppender("rollingLog", "RollingFile");
-
-        //
+        // 콘솔에 INFO 수준 로그가 기록되도록 루트 로거 구성
         RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.INFO);
         rootLogger.add(builder.newAppenderRef("stdout"));
         builder.add(rootLogger);
 
-        //
-        LoggerComponentBuilder addtionalLogger = builder.newLogger("", Level.ERROR)
+        // 롤링 로그에 DEBUG 수준 로그가 기록되도록 추가 로거 구성
+//        LoggerComponentBuilder debugLogger = builder.newLogger("", Level.DEBUG);
+//        debugLogger.add(builder.newAppenderRef("log"));
+//        builder.add(debugLogger);
 
+        try {
+            builder.writeXmlConfiguration(System.out);
 
+        } catch (IOException ie) {
+            System.out.println(" ");
+            return null;
+        }
+
+        System.out.println(" ");
         return builder.build();
+    }
+
+    @Override
+    protected String[] getSupportedTypes() {
+        return new String[] { "*" };
     }
 }
